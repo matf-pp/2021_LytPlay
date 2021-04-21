24561e111c66db5ea865bb27d7bdae7a3f0009ea
Min032 <asocial.bastard3@gmail.com>
2021-04-21 15:39:35 +0200

    formatting

--- src/utility.lua
+++ src/utility.lua
@@ -6,9 +6,11 @@ local pathToSong = pathToMusicDir .. "%(title)s.%(ext)s"
 
 function downloadSong(url)
     print("Downloading " .. url)
-    
     local command = "youtube-dl -x -o " .. "\"" .. pathToSong .. "\"" .. " " .. url
-
     os.execute(command)
+end
 
+function clearSongsCache()
+    print("Deleting cache...")
+    --for file in lfs.dir(pathToMusicDir) do
 end
